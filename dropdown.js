884d832bf2277ec4c7af16b447824336b4748057
Randy Doan <49592877+DoanRandy109@users.noreply.github.com>
2019-07-16 08:57:52 -0500

    Reverted back

--- dropdown.js
+++ dropdown.js
@@ -6,8 +6,8 @@ function myFunction() {
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.main-toggle active-element')) {
-    var dropdowns = document.getElementsByClassName("main-nav-list active-element");
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
